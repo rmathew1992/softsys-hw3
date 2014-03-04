2e11671a543b9d74b22f1510ad15f19e372c7294
Rachel Mathew <rachelmathew999@gmail.com>
2014-03-03 23:11:57 -0500

    Majority of parsing, hopefully, finished

---
+++ parse.py
@@ -0,0 +1,37 @@
+import string
+import re
+
+inputfile = open('test.list','r')
+outputfile = open('test.csv', 'w')
+# for i in range(4): inputfile.next() Use this when you want to skip the introductory stuff
+
+#Names start only with symbols,characters, and numbers
+char = []
+for i in range(33,127):
+		char.append(chr(i))
+full_list = {'temp':'movie'}
+#A parse function returns actor names
+actor = ['temp']
+def parse_actor(text):
+	temp = []
+	if text.startswith(tuple(char)):
+		columns = text.split("	",1)
+		for i in columns:
+			i = re.sub(r"\t", "", i)
+			temp.append(i.split())
+		actor[0] = ' '.join(temp.pop(0))
+		movie = ' '.join(temp.pop(0))
+		full_list[actor[0]] = [movie]
+	elif text.startswith('	'): 
+		text = text.strip()
+		full_list[actor[0]].append(text)
+
+for i in range(15336597):
+	if i < 240 or i > 15336597:
+		inputfile.next()
+	else:
+		line = inputfile.read
+		parse_actor(line)
+
+print full_list
+inputfile.close()
