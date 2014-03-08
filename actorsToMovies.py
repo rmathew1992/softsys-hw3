40fb49e0be5830d210a0d3feb261f8f5ed3b0111
alee123 <amanda@clypd.com>
2014-03-08 02:26:10 -0500

    comments for dbms stuff and readme

--- actorsToMovies.py
+++ actorsToMovies.py
@@ -1,9 +1,27 @@
+# March 2014
+# Amanda Lee, Rachel Mathew, Mike Warner
+
+# This program takes "actors.list" and "actresses.list" (with the extraneous lines 
+# removed beforehand) and parses the data to add to a database. For each actor/
+# actress the program parses out a list of movies (that are associated with a year)
+# that is also stored in the database. TV shows are not included. The end result of 
+# this program is a database called actorsToMovies that is indexed by actor name
+# and the movies (to make searching faster).
+
 import string
 import re
 import pymongo
 from pymongo import MongoClient
 from pymongo import ASCENDING
 
+# This function takes in a dictionary that was created during the parsing
+# process and searches through it for new entries to add to the mongo 
+# database. It makes sure to exclude the temporary entry that the dictionary
+# was initialized with. The dictionary entry is put into a new dictionary 
+# with different keys so that it can be inserted into mongo. 
+#
+# actorD			a dictionary with the key being the actor name
+#					and the value being a list of movies. 
 def dictToJson (actorD):
 	tempDict = {}
 	for key in actorD:
@@ -13,9 +31,16 @@ def dictToJson (actorD):
 
 			aToM.insert(tempDict)
 
+
+# This function takes the lines from the file and adds the movies as 
+# values for the corresponding actors. When the next actor is parsed
+# the finished actor is placed in the database. 
+#
+# text 				a string that is a line from file
+# dictionary		dictionary that holds that actor/movie data
+# initial 			the current actor that is being parsed
 def parse_actor(text, dictionary, initial):
 	movieToParse = text
-	#A parse function returns actor names
 	if text.startswith(tuple(char)):
 		dictToJson (dictionary)
 		del dictionary[initial[0]]
@@ -28,6 +53,12 @@ def parse_actor(text, dictionary, initial):
 	if (movie!="" and movie[0]!='"'):
 			dictionary[initial[0]].append(movie)
 
+
+# This function takes a line string (that includes an actor name) and
+# parses out the actor name and first movie they star in. It returns a 
+# tuple with a string that is the actor name and the movie title. 
+#
+# textLine			a string that is a line from the file. 
 def parseActorName (textLine):
 	temp = []
 	columns = textLine.split("	",1)
@@ -39,37 +70,62 @@ def parseActorName (textLine):
 	movie = ' '.join(temp.pop(0))
 	return actor, movie
 
+
+# This function takes a line string (that does not have an actor name)
+# and parses out the movie name. This is done by stripping all the white
+# space and cutting off the string after the year. It returns a string
+# that is the movie title with the year.
+#
+# textLine 			a string that is a line from the file
 def parseMovieName (textLine):
 	columns = textLine.split(")", 1)
 	movie = columns[0]
 	movie = movie.strip()
 	return movie
 
+
+# This function takes in a file name and a temporary dictionary.
+# The file is read line by line, and each line is passed to a parsing
+# function. It returns a dictionary. 
+#
+# file_name			a string that is the file name
+# parsed			a temporary dictionary that is passed directly
+#					to the parsing function
 def read_file(file_name, parsed):
 	inputfile = open(file_name,'r')
 	initial = ['temp']
 	for line in inputfile.readlines():
+
+		# the line string needs to be decoded to replace characters
+		# that are not valid and cannot be added to the database
 		rawLine = line.decode('utf-8', 'replace')
 		finalLine = rawLine.encode('utf-8')
+
 		parse_actor(finalLine, parsed, initial)
 	inputfile.close()
 	return parsed
 
 
+# starts the connection to Mongo
 connection = MongoClient()
 db = connection.sofSys3
+
+# resets the actorsToMovies collection and accesses it
 connection['sofSys3'].drop_collection('actorsToMovies')
 aToM = db.actorsToMovies
+
+# indexing for the collection
 aToM.create_index([("actor", ASCENDING)])
 aToM.ensure_index("movies")
-#Names start only with symbols,characters, and numbers
+
+# Names start only with symbols,characters, and numbers
 char = []
 for i in range(33,127):
 	char.append(chr(i))
 
+# temporary dictionaries to pass into read_file function
 actors = {'temp':'movie'}
 actresses = {'temp': 'movie'}
-
 
 # List files must have excess information trimmed out
 actors = read_file('actors.list', actors)
--- moviesToActors.py
+++ moviesToActors.py
@@ -1,9 +1,25 @@
+# March 2014
+# Amanda Lee, Rachel Mathew, Mike Warner
+
+# When this python program is run, it will look at a file called "movies.list" 
+# that is downloaded from the imdb site. Before we ran this program, we deleted 
+# extrangeous lines from the beginning and end. The program will take this list
+# and search through the actorsToMovies database to find all actors that star in
+# a specific movie. When the program is done running, there will be a moviesToActors
+# database that maps movie titles to a list of the cast. 
+
 import string
 import re
 import pymongo
 from pymongo import MongoClient
 from pymongo import ASCENDING
 
+# This function takes a movie name and searches the actorsToMovies database
+# to find all actors that have starred in that movie. These actors are 
+# put in a list and inserted into a new mongo database that maps movies to 
+# their casts.
+# 
+# movie 				a string representing the movie title
 def findActors (movie):
 	actors = aToM.find({"movies": movie}, fields={'_id':False, 'movies':False})
 	tempDict = {}
@@ -14,18 +30,28 @@ def findActors (movie):
 	tempDict["actors"] = actorList
 	mToA.insert(tempDict)
 
-
+# This function parses out the movie from a string.
+#
+# textLine				a string representing a line of text
 def parseMovieName (textLine):
 	columns = textLine.split(")", 1)
 	movie = columns[0]
 	movie = movie.strip()
 	return movie
 
+# The function takes a line of text and checks if it is a TV show or 
+# a movie. If it is a movie, it is parsed and added to the database
+#
+# text 					a string representing a line of text
 def parse_Movie(text):
 	if (text[0]!='"'):
 		movieTitle = parseMovieName(text)
 		findActors(movieTitle)
 
+# This function reads a given file line by line and feeds the lines to
+# a parser. 
+# 
+# file_name				a string that represents the filename
 def read_file(file_name):
 	inputfile = open(file_name,'r')
 	for line in inputfile.readlines():
@@ -34,10 +60,12 @@ def read_file(file_name):
 		parse_Movie(finalLine)
 	inputfile.close()
 
-
+# opens the connection to the Mongo database
 connection = MongoClient()
 db = connection.sofSys3
 aToM = db.actorsToMovies
+
+# resets the moviesToActors database 
 connection['sofSys3'].drop_collection('moviesToActors')
 mToA = db.moviesToActors
 
